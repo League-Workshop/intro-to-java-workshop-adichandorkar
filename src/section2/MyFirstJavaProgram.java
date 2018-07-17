c311a3b5642348221db69fdeff615c1be1fee534
League <league@WTS8.attlocal.net>
2018-07-17 11:57:18 -0700

    day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -4,10 +4,18 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot banana = new Robot();
+	banana.setSpeed(200 );
+	banana.setPenColor(Color.RED);
+	banana.penDown();
+	for(int i=0;i < 4;i++) {
+		banana.move(100);
+		banana.turn(90);
+	}
 	
 		
 		
