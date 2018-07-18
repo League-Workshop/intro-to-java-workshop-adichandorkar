65828177a3c038b75f78344577f8b17271229451
League <league@WTS8.attlocal.net>
2018-07-18 11:58:28 -0700

    day 3

---
+++ src/section3/NicestProgram.java
@@ -0,0 +1,12 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "you are fast at programming");
+		JOptionPane.showMessageDialog(null, "I like you're t-shirt");
+		JOptionPane.showMessageDialog(null, "I like youre hair style");
+		
+	}
+}
